6d65582d6eec29ecfff317fb5e2da89b00651720
safinfa <Safinfarrashidiq@gmail.com>
2023-12-22 17:34:08 +0700

    test

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // copy menu for mobile 
 function copyMenu() {
-    var dptCat = document.querySelector('.dptCat');
+    var dptCategory = document.querySelector('.dpt-cat');
+    var dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    //copy inside nav to nav
+    var mainNav = document.querySelector('.header-nav nav');
+    var navPlace = document.querySelector('.off-canvas nav');
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    //copy .header-top .warpper to .thetop-nav
+    var topNav = document.querySelector('.header-top');
+    var topPlace = document.querySelector('.off-canvas .thetop-nav');
+    topPlace.innerHTML = topNav.innerHTML;
 }
+copyMenu();
