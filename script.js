1d84ec442e877ba9c828dc1536cb7cd4c342ed1c
safinfa <Safinfarrashidiq@gmail.com>
2023-12-23 18:39:31 +0700

    test

--- script.js
+++ script.js
@@ -40,3 +40,14 @@ function toggle(e) {
     if (this.closest('.has-child').classList != 'expand');
     this.closest('.has-child').classList.toggle('expand');
 }
+
+//slider
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+  });
